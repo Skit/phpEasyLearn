52f71b483128a247e5cfdf96c49cba94d1dc88d1
Skit <wabto@mail.ru>
2020-06-02 01:33:05 +0300

    step_1

---
+++ start.php
@@ -0,0 +1,21 @@
+<?php
+  // цифры
+  $number = 2;
+  $number_2 = 2;
+
+  // строки
+  $string = "Hello";
+  $string_2 = "World";
+  $string_3 = date("d.m.Y H:i:s");
+
+  // вывод на экран
+  print $number + $number;
+  // перенос строки
+  print PHP_EOL;
+  // еще вариант вывода на экран
+  echo $string_3;
+
+  echo PHP_EOL;
+
+  echo $string . ' ' . $string_2 . PHP_EOL;
+?>
